a8c2dd2aaa021f5cb00b9ae2f6d14d12daca13ca
noppa <the.nobba@gmail.com>
2019-03-16 19:43:38 +0200

    Basic test setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-export default function(babel) {}
+import * as Babel from '@babel/core';
+
+export default function(): Babel.PluginObj<{}> {
+	return {
+		visitor: {
+			Identifier(path) {
+				const name = path.node.name;
+				// reverse the name: JavaScript -> tpircSavaJ
+				path.node.name = name
+					.split('')
+					.reverse()
+					.join('');
+			},
+		},
+	};
+}
