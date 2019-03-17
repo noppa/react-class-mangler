7c26961f994752dcceda4760ea2f1012a42070af
noppa <the.nobba@gmail.com>
2019-03-17 03:45:09 +0200

    Handle object destructuring

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import * as Babel from '@babel/core';
 import { NodePath } from '@babel/traverse';
 import * as t from '@babel/types';
-import { getWithDefault } from 'get-optional';
+import { get } from 'get-optional';
 import publicComponentApi from './public-component-api';
 
 type RenameConfig = {
@@ -24,11 +24,11 @@ function VisitMember(path: NodePath<Member>, state: VisitorState) {
 		return;
 	}
 
-	const id = path.scope.generateUidIdentifier('a');
+	const id = path.scope.generateUid('c').replace('_', '');
 	state.renames.set(key.name, {
-		newName: id.name,
+		newName: id,
 	});
-	path.node.key = id;
+	key.name = id;
 }
 
 function VisitClass(path: NodePath<ClassExprDecl>) {
@@ -37,7 +37,7 @@ function VisitClass(path: NodePath<ClassExprDecl>) {
 	}
 
 	const state: VisitorState = {
-		className: getWithDefault('unknown', path.node.id, 'name'),
+		className: get(path.node.id, 'name'),
 		renames: new Map(),
 	};
 	path.traverse(classVisitor, state);
@@ -52,6 +52,18 @@ function isReactClass({ superClass }: ClassExprDecl) {
 	);
 }
 
+function renameIdentifier(property: t.Identifier, state: VisitorState) {
+	const rename = state.renames.get(property.name);
+	// TODO: Allow properties to be declared in methods.
+	if (!rename) {
+		return;
+	}
+	console.log(
+		`Renaming ${state.className}.${property.name} to ${rename.newName}`
+	);
+	property.name = rename.newName;
+}
+
 function VisitMemberExpr(
 	path: NodePath<t.MemberExpression>,
 	state: VisitorState
@@ -64,26 +76,50 @@ function VisitMemberExpr(
 	if (node.object.type !== 'ThisExpression' || property.type !== 'Identifier') {
 		return;
 	}
-	const rename = state.renames.get(property.name);
-	console.log('member expr', property.name, rename);
-	// TODO: Allow properties to be declared in methods.
-	if (!rename) {
+	renameIdentifier(property, state);
+}
+
+function VisitVarDecl(
+	path: NodePath<t.VariableDeclarator>,
+	state: VisitorState
+) {
+	const { node } = path;
+	if (
+		!node.init ||
+		node.init.type !== 'ThisExpression' ||
+		node.id.type !== 'ObjectPattern'
+	) {
 		return;
 	}
-	property.name = rename.newName;
+
+	for (const prop of node.id.properties) {
+		// TODO: Handle RestElement
+		if (prop.type === 'RestElement') {
+			continue;
+		}
+		renameIdentifier(prop.key, state);
+		if (prop.value.type === 'Identifier' && prop.shorthand) {
+			// TODO: Handle (bail?) other types
+			// const newName = path.scope.generateUid(prop.key.name);
+			const newName = prop.key.name;
+			path.scope.rename(prop.value.name, newName);
+		}
+	}
 }
 
 const classVisitor: Babel.Visitor<VisitorState> = {
 	ClassMethod: VisitMember,
 	ClassProperty: VisitMember,
 	MemberExpression: VisitMemberExpr,
+	VariableDeclarator: VisitVarDecl,
 };
 
 export default function(): Babel.PluginObj<VisitorState> {
 	return {
 		visitor: {
 			ClassDeclaration: VisitClass,
-			ClassExpression: VisitClass,
+			// TODO: Don't change property names twice
+			// ClassExpression: VisitClass,
 		},
 	};
 }
--- test/demoproject/src/basic-component.js
+++ test/demoproject/src/basic-component.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 
 class BasicComponent extends Component {
+	foo = 5;
 	greetings(i) {
 		return 'Hello' + (--i ? this.greetings(i) : '');
 	}
 	render() {
-		return <div>{this.greetings(5)}</div>;
+		const { foo } = this;
+		return (
+			<div>
+				{this.greetings(5)} {foo}
+			</div>
+		);
 	}
 }
 
